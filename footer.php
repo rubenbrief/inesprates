73b21f633890f523895cd3380c73c7b1ec61ab6d
Ruben Costa <ruben@brief.pt>
2017-06-08 23:35:47 +0100

    footer mobile

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 			
-
+		</div><!--container-->
 		<footer id="footer" class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
 
